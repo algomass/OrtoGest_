6ed501a7ddb5a435ad060a187c2c017d50d93a63
algomass <algozzinomatteo@gmail.com>
2026-06-03 12:45:38 +0200

    duplication pt3

--- src/main/java/it/ortogest/ortogestapp/model/AbstractLotto.java
+++ src/main/java/it/ortogest/ortogestapp/model/AbstractLotto.java
@@ -15,6 +15,44 @@ public abstract class AbstractLotto {
 
     public AbstractLotto() {}
 
+    protected AbstractLotto(AbstractBuilder<?, ?> builder) {
+        this.idLotto = builder.idLotto;
+        this.nomeFornitore = builder.nomeFornitore;
+        this.quantitaKg = builder.quantitaKg;
+        this.dataArrivo = builder.dataArrivo;
+        this.dataScadenza = builder.dataScadenza;
+        this.costoAcquisto = builder.costoAcquisto;
+        this.prezzoVendita = builder.prezzoVendita;
+        this.scontoScadenzaAttivo = builder.scontoScadenzaAttivo;
+        this.prezzoScontato = builder.prezzoScontato;
+    }
+
+    public abstract static class AbstractBuilder<T extends AbstractLotto, B extends AbstractBuilder<T, B>> {
+        protected String idLotto;
+        protected String nomeFornitore;
+        protected double quantitaKg;
+        protected LocalDate dataArrivo;
+        protected LocalDate dataScadenza;
+        protected double costoAcquisto;
+        protected double prezzoVendita;
+        protected boolean scontoScadenzaAttivo;
+        protected double prezzoScontato;
+
+        protected abstract B self();
+
+        public abstract T build();
+
+        public B idLotto(String idLotto) { this.idLotto = idLotto; return self(); }
+        public B nomeFornitore(String nomeFornitore) { this.nomeFornitore = nomeFornitore; return self(); }
+        public B quantitaKg(double quantitaKg) { this.quantitaKg = quantitaKg; return self(); }
+        public B dataArrivo(LocalDate dataArrivo) { this.dataArrivo = dataArrivo; return self(); }
+        public B dataScadenza(LocalDate dataScadenza) { this.dataScadenza = dataScadenza; return self(); }
+        public B costoAcquisto(double costoAcquisto) { this.costoAcquisto = costoAcquisto; return self(); }
+        public B prezzoVendita(double prezzoVendita) { this.prezzoVendita = prezzoVendita; return self(); }
+        public B scontoScadenzaAttivo(boolean scontoScadenzaAttivo) { this.scontoScadenzaAttivo = scontoScadenzaAttivo; return self(); }
+        public B prezzoScontato(double prezzoScontato) { this.prezzoScontato = prezzoScontato; return self(); }
+    }
+
     public String getIdLotto() {
         return idLotto;
     }
--- src/main/java/it/ortogest/ortogestapp/model/Lotto.java
+++ src/main/java/it/ortogest/ortogestapp/model/Lotto.java
@@ -10,46 +10,28 @@ public class Lotto extends AbstractLotto {
     private Prodotto tipologiaProdotto;
 
     private Lotto(Builder builder) {
-        super();
-        this.setIdLotto(builder.idLotto);
-        this.setNomeFornitore(builder.nomeFornitore);
+        super(builder);
         this.tipologiaProdotto = builder.tipologiaProdotto;
-        this.setQuantitaKg(builder.quantitaKg);
-        this.setDataArrivo(builder.dataArrivo);
-        this.setDataScadenza(builder.dataScadenza);
-        this.setCostoAcquisto(builder.costoAcquisto);
-        this.setPrezzoVendita(builder.prezzoVendita);
-        this.setScontoScadenzaAttivo(builder.scontoScadenzaAttivo);
-        this.setPrezzoScontato(builder.prezzoScontato);
     }
 
     public static Builder builder() {
         return new Builder();
     }
 
-    public static class Builder {
-        private String idLotto;
-        private String nomeFornitore;
+    public static class Builder extends AbstractLotto.AbstractBuilder<Lotto, Builder> {
         private Prodotto tipologiaProdotto;
-        private double quantitaKg;
-        private LocalDate dataArrivo;
-        private LocalDate dataScadenza;
-        private double costoAcquisto;
-        private double prezzoVendita;
-        private boolean scontoScadenzaAttivo;
-        private double prezzoScontato;
 
-        public Builder idLotto(String idLotto) { this.idLotto = idLotto; return this; }
-        public Builder nomeFornitore(String nomeFornitore) { this.nomeFornitore = nomeFornitore; return this; }
-        public Builder tipologiaProdotto(Prodotto tipologiaProdotto) { this.tipologiaProdotto = tipologiaProdotto; return this; }
-        public Builder quantitaKg(double quantitaKg) { this.quantitaKg = quantitaKg; return this; }
-        public Builder dataArrivo(LocalDate dataArrivo) { this.dataArrivo = dataArrivo; return this; }
-        public Builder dataScadenza(LocalDate dataScadenza) { this.dataScadenza = dataScadenza; return this; }
-        public Builder costoAcquisto(double costoAcquisto) { this.costoAcquisto = costoAcquisto; return this; }
-        public Builder prezzoVendita(double prezzoVendita) { this.prezzoVendita = prezzoVendita; return this; }
-        public Builder scontoScadenzaAttivo(boolean scontoScadenzaAttivo) { this.scontoScadenzaAttivo = scontoScadenzaAttivo; return this; }
-        public Builder prezzoScontato(double prezzoScontato) { this.prezzoScontato = prezzoScontato; return this; }
+        @Override
+        protected Builder self() {
+            return this;
+        }
 
+        public Builder tipologiaProdotto(Prodotto tipologiaProdotto) { 
+            this.tipologiaProdotto = tipologiaProdotto; 
+            return this; 
+        }
+
+        @Override
         public Lotto build() {
             return new Lotto(this);
         }
